e38d9fb3920e51ff60f68a9b79a8fd23815552ad
eseospku <eseosapku@gmail.com>
2022-07-07 09:45:38 -0700

    commit

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
 print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[:-2]}")
-print(f"Middle word: {word[4:6]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[2:8]}")
