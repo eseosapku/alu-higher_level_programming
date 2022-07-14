fdbdc6c4b997e09dc3e0034022fd9eede4356988
eseospku <eseosapku@gmail.com>
2022-07-14 14:53:58 -0700

    commit

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# function computes the square value of all integers of a matrix
+
 
 def square_matrix_simple(matrix=[]):
     new_matrix = list()
@@ -7,4 +9,4 @@ def square_matrix_simple(matrix=[]):
         for temp_two in temp_one:
             temp_matrix.append(temp_two**2)
         new_matrix.append(temp_matrix)
-    return
+    return 
