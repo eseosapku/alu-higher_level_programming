cd380973c9c7a6d7261f75a98833c5f2e299f89a
eseospku <eseosapku@gmail.com>
2022-07-22 15:42:35 -0700

    commit

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    print_count = 0
+def safe_print_integer(value):
     try:
-        for i in range(x):
-            if type(my_list[i]) != int:
-                continue
-            else:
-                print("{:d}".format(my_list[i]), end="")
-                print_count += 1
-        print()
-        return print_count
-    except TypeError:
-        pass
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
