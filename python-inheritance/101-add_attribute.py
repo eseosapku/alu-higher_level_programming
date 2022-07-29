b82d2aa7425f34249c5c24d4566b3ed54b0fa040
eseospku <eseosapku@gmail.com>
2022-07-29 02:21:01 -0700

    commit

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -9,4 +9,4 @@ def add_attribute(ob, attr, value):
     if hasattr(ob, "__dict__"):
         setattr(ob, attr, value)
     else:
-        raise TypeError("can't add new attribute"
+        raise TypeError("can't add new attribute")
