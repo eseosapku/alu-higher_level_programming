e9c4db79abad33f13abbdcf5e852ba62132afd7b
eseosapku <eseosapku@gmail.com>
2022-08-05 09:20:28 +0000

    commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-""" module uses another module to request to make request """
-import urllib.request
+"""A script that
+fetches https://intranet.hbtn.io/status.
+"""
 
 
-"""making request to provided url"""
-if __name__ == "__main__":
-    """making request to provided url"""
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        """ the method i used is easy way for string manipulation 
-            to use a method that make use of the concept
-            print out html.decode('utf-8') with string formatting
-        """
-        temp = str(html)
-        print("\t- utf8 content: {}".format(temp[2:-1]))
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        content = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
