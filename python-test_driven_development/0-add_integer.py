9038413bd562cdee575ec0e50502cfcb78943412
eseospku <eseosapku@gmail.com>
2022-08-25 08:54:46 -0700

    commit

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
 """
-    add 2 different
-    integers
-
+    addition of
+    two integers
 """
+
 
 def add_integer(a=0, b=98):
-    """ function """
+    """ function adds integers """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
