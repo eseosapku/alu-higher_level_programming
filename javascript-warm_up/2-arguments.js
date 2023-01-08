9224e4229359f57e24a87757fca47d1aedebfea5
eseosapku <e.uwagboe@alustudent.com>
2023-01-08 12:01:25 -0800

    commit

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length === 3) {
   console.log('Argument found');
+} if (process.argv.length === 0) {
+  console.log('No argument');
 } else {
   console.log('Arguments found');
 }
