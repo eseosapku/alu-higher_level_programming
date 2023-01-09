2634aaaf7ee1dc766605c23ddb18fbe58bd03232
eseosapku <e.uwagboe@alustudent.com>
2023-01-08 23:23:56 -0800

    commit

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-i = defined
-for (i in array) {
+let i = 0
+for (i in array; i++) {
   console.log(array[i]);
 }
