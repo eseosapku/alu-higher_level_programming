a721ea1c19cb8f85122ee94525b7207b006ba832
eseosapku <e.uwagboe@alustudent.com>
2023-01-09 00:34:19 -0800

    commit

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const number = process.argv[2];
+  let i = 0;
+  while (i < number) {
+    console.log('x' * number);
+    i++;
+  }
+}
